aa563de4e918e192883aa34e072d18a522d918b5
BenTaylor-TfN (Sneezy) <benjamin.taylor@transportforthenorth.com>
2022-08-17 13:03:56 +0100

    src-layout test

--- src/caf/toolkit/__init__.py
+++ src/caf/toolkit/__init__.py
@@ -1,6 +1,8 @@
-
-
 from . import _version
 __version__ = _version.get_versions()['version']
 
+# Alias
 from caf.toolkit import pandas_utils
+
+from . import _version
+__version__ = _version.get_versions()['version']
--- src/caf/toolkit/_version.py
+++ src/caf/toolkit/_version.py
@@ -45,7 +45,7 @@ def get_config():
     cfg.style = "pep440-post-branch"
     cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "None"
-    cfg.versionfile_source = "caf/toolkit/_version.py"
+    cfg.versionfile_source = "src/caf/toolkit/_version.py"
     cfg.verbose = False
     return cfg
 
