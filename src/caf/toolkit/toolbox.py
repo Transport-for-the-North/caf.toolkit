90cecbdd8fc6838b1cacf767f1a57875e6990438
BenTaylor-TfN (Sneezy) <benjamin.taylor@transportforthenorth.com>
2023-04-14 16:06:39 +0100

    Update toolbox.py

--- src/caf/toolkit/toolbox.py
+++ src/caf/toolkit/toolbox.py
@@ -139,6 +139,39 @@ def get_missing_items(list_a: list[_T], list_b: list[_T]) -> tuple[list[_T], lis
     return list(a_not_b), list(b_not_a)
 
 
+def compare_sets(set_a: set[_T], set_b: set[_T]) -> tuple[bool, set[_T], set[_T]]:
+    """
+    Check whether set_a and x2 are the same.
+
+    Checks for items that are in x1, and not x2.
+    Checks for items that are in x2 and not x1.
+    If both above checks are empty, then sets must be equal.
+
+    Parameters
+    ----------
+    set_a:
+        The first set the check
+
+    set_b:
+        The second set the check
+
+    Returns
+    -------
+    equal:
+        True if set_a and set_b are equal, otherwise False.
+
+    a_not_in_b:
+        A set of items that are in set_a, but not in set_b.
+
+    b_not_in_a:
+        A set of items that are in set_b, but not in set_a.
+    """
+    a_not_in_b = set_a - set_b
+    b_not_in_a = set_b - set_a
+    equal = len(a_not_in_b) == 0 and len(b_not_in_a) == 0
+    return equal, a_not_in_b, b_not_in_a
+
+
 # TODO(BT): Can this take a Collection instead?
 def is_unique_list(unique_vals: list[Any]) -> bool:
     """Check whether a list contains unique values only.
