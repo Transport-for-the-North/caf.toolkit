cd9b98a3917dbb8c468501707479103349388b0c
Matt Buckley <matthew.buckley@wsp.com>
2023-07-14 11:05:46 +0100

    Added LogHelper class
    Added `LogHelper` class for creating and managing loggers.
    Added `SystemInformation` class for loading PC and user info.
    Added `ToolDetails` class for handling tool information.

--- src/caf/toolkit/__init__.py
+++ src/caf/toolkit/__init__.py
@@ -5,3 +5,4 @@ __version__ = _version.get_versions()["version"]
 # Alias
 from caf.toolkit import pandas_utils
 from caf.toolkit.config_base import BaseConfig
+from caf.toolkit.log_helpers import LogHelper, TemporaryLogFile, ToolDetails, SystemInformation
--- src/caf/toolkit/log_helpers.py
+++ src/caf/toolkit/log_helpers.py
@@ -1,16 +1,29 @@
 # -*- coding: utf-8 -*-
-"""Helper functions for creating and managing a logger."""
+"""
+Helper functions for creating and managing a logger.
+
+Classes
+-------
+LogHelper
+    Main context manager class for creating and managing loggers and
+    logging key tool / system information.
+TemporaryLogFile
+    Context manager for adding a log file handler to a logger and
+    removing it when done.
+"""
 from __future__ import annotations
 
 # Built-Ins
-import os
+import functools
+import getpass
 import logging
-
-from typing import Any
-from typing import Iterable
-from typing import Optional
+import os
+import platform
+from typing import Any, Iterable, Optional
 
 # Third Party
+import pydantic
+from pydantic import dataclasses
 
 # Local Imports
 # pylint: disable=import-error,wrong-import-position
@@ -18,12 +31,281 @@ from typing import Optional
 # pylint: enable=import-error,wrong-import-position
 
 # # # CONSTANTS # # #
-LOG = logging.getLogger(__name__)
+DEFAULT_CONSOLE_FORMAT = "[%(asctime)s - %(levelname)-8.8s] %(message)s"
+DEFAULT_CONSOLE_DATETIME = "%H:%M:%S"
+DEFAULT_FILE_FORMAT = "%(asctime)s [%(name)-40.40s] [%(levelname)-8.8s] %(message)s"
+DEFAULT_FILE_DATETIME = "%d-%m-%Y %H:%M:%S"
+
 
 # # # CLASSES # # #
+@dataclasses.dataclass
+class ToolDetails:
+    """Information about the current tool.
+
+    Attributes
+    ----------
+    name: str
+        Name of the tool.
+    version: str
+        Version of the tool.
+    homepage: str, optional
+        URL of the homepage for the tool.
+    source_url: str, optional
+        URL of the source code repository for the tool.
+    """
+
+    name: str
+    version: str
+    homepage: Optional[pydantic.HttpUrl] = None
+    source_url: Optional[pydantic.HttpUrl] = None
+
+    def __str__(self) -> str:
+        """Nicely formatted multi-line string."""
+        message = ["Tool Information", "----------------"]
+
+        # pylint false positive for __dataclass_fields__ no-member
+        # pylint: disable=no-member
+        length = functools.reduce(max, (len(i) for i in self.__dataclass_fields__))
+
+        for name in self.__dataclass_fields__:
+            message.append(f"{name:<{length}.{length}} : {getattr(self, name)}")
+
+        return "\n".join(message)
+
+
+@dataclasses.dataclass
+class SystemInformation:
+    """Information about the PC and Python version.
+
+    Attributes
+    ----------
+    user: str
+        Account name of the currenly logged in user.
+    pc_name: str
+        Name of the PC.
+    python_version: str
+        Python version being used.
+    operating_system: str
+        Information about the name and version of OS.
+    architecture: str
+        Name of the machine architecture e.g. "AMD64".
+    """
+
+    user: str
+    pc_name: str
+    python_version: str
+    operating_system: str
+    architecture: str
+
+    @classmethod
+    def load(cls) -> SystemInformation:
+        """Load system information."""
+        info = platform.uname()
+
+        return SystemInformation(
+            user=getpass.getuser(),
+            pc_name=info.node,
+            python_version=platform.python_version(),
+            operating_system=f"{info.system} {info.release} ({info.version})",
+            architecture=info.machine,
+        )
+
+    def __str__(self) -> str:
+        """Nicely formatted multi-line string."""
+        message = ["System Information", "------------------"]
+
+        # pylint false positive for __dataclass_fields__ no-member
+        # pylint: disable=no-member
+        length = functools.reduce(max, (len(i) for i in self.__dataclass_fields__))
+
+        for name in self.__dataclass_fields__:
+            message.append(f"{name:<{length}.{length}} : {getattr(self, name)}")
+
+        return "\n".join(message)
+
+
+class LogHelper:
+    """Class for managing Python loggers."""
+
+    # TODO(MB) Add examples to docstring
+
+    def __init__(
+        self,
+        root_logger: str,
+        tool_details: ToolDetails,
+        console: bool = True,
+        log_file: os.PathLike | None = None,
+    ):
+        self.logger_name = str(root_logger)
+        self.logger = logging.getLogger(self.logger_name)
+        self.logger.setLevel(logging.DEBUG)
+
+        self.tool_details = tool_details
+
+        if console:
+            handler = get_console_handler()
+            self.logger.addHandler(handler)
+
+        if log_file is not None:
+            handler = get_file_handler(log_file)
+            self.logger.addHandler(handler)
+
+        self.write_instantiate_message()
+
+    def add_console_handler(
+        self,
+        ch_format: str = DEFAULT_CONSOLE_FORMAT,
+        datetime_format: str = DEFAULT_CONSOLE_DATETIME,
+        log_level: int = logging.INFO,
+    ) -> None:
+        """Wrapper around `get_console_handler` to add custom console handlers.
+
+        Parameters
+        ----------
+        ch_format:
+            A string defining a custom formatting to use for the StreamHandler().
+            Defaults to "[%(levelname)-8.8s] %(message)s".
+
+        datetime_format:
+            The datetime format to use when logging to the console.
+            Defaults to "%H:%M:%S"
+
+        log_level:
+            The logging level to give to the StreamHandler.
+        """
+        handler = get_console_handler(ch_format, datetime_format, log_level)
+        self.logger.addHandler(handler)
+
+    def add_file_handler(
+        self,
+        log_file: os.PathLike,
+        fh_format: str = DEFAULT_FILE_FORMAT,
+        datetime_format: str = DEFAULT_FILE_DATETIME,
+        log_level: int = logging.DEBUG,
+    ) -> None:
+        """Wrapper around `get_file_handler` to add custom file handlers.
+
+        Parameters
+        ----------
+        log_file:
+            The path to a file to output the log
+
+        fh_format:
+            A string defining a custom formatting to use for the StreamHandler().
+            Defaults to
+            "%(asctime)s [%(name)-40.40s] [%(levelname)-8.8s] %(message)s".
+
+        datetime_format:
+            The datetime format to use when logging to the console.
+            Defaults to "%d-%m-%Y %H:%M:%S"
+
+        log_level:
+            The logging level to give to the FileHandler.
+        """
+        handler = get_file_handler(log_file, fh_format, datetime_format, log_level)
+        self.logger.addHandler(handler)
+
+    def capture_warnings(self) -> None:
+        """Capture warnings using logging.
+
+        Runs `logging.captureWarnings(True)` to capture warnings then
+        adds all the handlers from the root `logger`.
+        """
+        logging.captureWarnings(True)
+
+        warning_logger = logging.getLogger("py.warnings")
+
+        for handler in self.logger.handlers:
+            if handler in warning_logger.handlers:
+                continue
+
+            warning_logger.addHandler(handler)
+
+    def write_instantiate_message(self) -> None:
+        """Write instatiation message with tool / system information."""
+        write_instantiate_message(self.logger, self.tool_details.name)
+        write_information(self.logger, self.tool_details)
+
+    def __enter__(self):
+        """Called when initialising class with 'with' statement."""
+        return self
+
+    def __exit__(self, excepType, excepVal, traceback):
+        """Called when exiting with statement, writes any error to the logger and closes the file."""
+        if excepType is not None or excepVal is not None or traceback is not None:
+            self.logger.critical("Oh no a critical error occurred", exc_info=True)
+        else:
+            self.logger.info("Program completed without any critical errors")
+
+        self.logger.info("Closing log file")
+        logging.shutdown()
+
+
+class TemporaryLogFile:
+    """Add temporary log file to a logger."""
+
+    # TODO(MB) Add examples to docstring
+
+    def __init__(self, logger: logging.Logger, log_file: os.PathLike, **kwargs) -> None:
+        """Add temporary log file handler to `logger`.
+
+        Parameters
+        ----------
+        logger : logging.Logger
+            Logger to add FileHandler to.
+
+        log_file : nd.PathLike
+            Path to new log file to create.
+
+        kwargs : Keyword arguments, optional
+            Any arguments to pass to `get_file_handler`.
+        """
+        self.logger = logger
+        self.log_file = log_file
+        self.logger.debug('Creating temporary log file: "%s"', self.log_file)
+        self.handler = get_file_handler(log_file, **kwargs)
+        self.logger.addHandler(self.handler)
+        # TODO(MB) Log location of base log file
+
+    def __enter__(self) -> TemporaryLogFile:
+        """Initialise TemporaryLogFile."""
+        return self
+
+    def __exit__(self, excepType, excepVal, traceback) -> None:
+        """Close temporary log file."""
+        # pylint: disable=invalid-name
+        if excepType is not None or excepVal is not None or traceback is not None:
+            self.logger.critical("Oh no a critical error occurred", exc_info=True)
+
+        self.logger.removeHandler(self.handler)
+        self.logger.debug('Closed temporary log file: "%s"', self.log_file)
 
 
 # # # FUNCTIONS # # #
+def write_information(
+    logger: logging.Logger, tool_details: ToolDetails | None = None, system_info: bool = True
+) -> None:
+    """Write tool and system information to `logger`.
+
+    _extended_summary_
+
+    Parameters
+    ----------
+    logger : logging.Logger
+        Logger to write to
+    tool_details : ToolDetails, optional
+        Tool details to write to logger, not written if None.
+    system_info : bool, default True
+        Whether, or not, to load `SystemInformation` and write to logger.
+    """
+    if tool_details is not None:
+        logger.info("\n%s", tool_details)
+
+    if system_info:
+        info = SystemInformation.load()
+        logger.debug("\n%s", info)
+
+
 def write_instantiate_message(
     logger: logging.Logger,
     instantiate_message: str,
@@ -172,8 +454,8 @@ def get_logger(
 
 
 def get_console_handler(
-    ch_format: Optional[str] = None,
-    datetime_format: Optional[str] = None,
+    ch_format: str = DEFAULT_CONSOLE_FORMAT,
+    datetime_format: str = DEFAULT_CONSOLE_DATETIME,
     log_level: int = logging.INFO,
 ) -> logging.StreamHandler:
     """Create a console handles for a logger.
@@ -196,13 +478,6 @@ def get_console_handler(
     console_handler:
         A logging.StreamHandler object using the format in ch_format.
     """
-    if ch_format is None:
-        ch_format = "[%(asctime)s - %(levelname)-8.8s] %(message)s"
-
-    if datetime_format is None:
-        datetime_format = "%H:%M:%S"
-
-    # Create console handler
     handler = logging.StreamHandler()
     handler.setLevel(log_level)
     handler.setFormatter(logging.Formatter(ch_format, datefmt=datetime_format))
@@ -211,8 +486,8 @@ def get_console_handler(
 
 def get_file_handler(
     log_file: os.PathLike,
-    fh_format: Optional[str] = None,
-    datetime_format: Optional[str] = None,
+    fh_format: str = DEFAULT_FILE_FORMAT,
+    datetime_format: str = DEFAULT_FILE_DATETIME,
     log_level: int = logging.DEBUG,
 ) -> logging.StreamHandler:
     """Create a console handles for a logger.
@@ -232,21 +507,13 @@ def get_file_handler(
         Defaults to "%d-%m-%Y %H:%M:%S"
 
     log_level:
-        The logging level to give to the StreamHandler.
+        The logging level to give to the FileHandler.
 
     Returns
     -------
     console_handler:
         A logging.StreamHandler object using the format in ch_format.
     """
-    # Init
-    if fh_format is None:
-        fh_format = "%(asctime)s [%(name)-40.40s] [%(levelname)-8.8s] %(message)s"
-
-    if datetime_format is None:
-        datetime_format = "%d-%m-%Y %H:%M:%S"
-
-    # Create console handler
     handler = logging.FileHandler(log_file)
     handler.setLevel(log_level)
     handler.setFormatter(logging.Formatter(fh_format, datefmt=datetime_format))
@@ -287,40 +554,3 @@ def capture_warnings(
 
     if file_handler_args is not None:
         warning_logger.addHandler(get_file_handler(**file_handler_args))
-
-
-class TemporaryLogFile:
-    """Add temporary log file to a logger."""
-
-    def __init__(self, logger: logging.Logger, log_file: os.PathLike, **kwargs) -> None:
-        """Add temporary log file handler to `logger`.
-
-        Parameters
-        ----------
-        logger : logging.Logger
-            Logger to add FileHandler to.
-
-        log_file : nd.PathLike
-            Path to new log file to create.
-
-        kwargs : Keyword arguments, optional
-            Any arguments to pass to `get_file_handler`.
-        """
-        self.logger = logger
-        self.log_file = log_file
-        self.logger.debug('Creating temporary log file: "%s"', self.log_file)
-        self.handler = get_file_handler(log_file, **kwargs)
-        self.logger.addHandler(self.handler)
-
-    def __enter__(self) -> TemporaryLogFile:
-        """Initialise TemporaryLogFile."""
-        return self
-
-    def __exit__(self, excepType, excepVal, traceback) -> None:
-        """Close temporary log file."""
-        # pylint: disable=invalid-name
-        if excepType is not None or excepVal is not None or traceback is not None:
-            self.logger.critical("Oh no a critical error occurred", exc_info=True)
-
-        self.logger.removeHandler(self.handler)
-        self.logger.debug('Closed temporary log file: "%s"', self.log_file)
