c04275da3fcaa426c8acec31df7d7d0965bfba51
BenTaylor-TfN (Sneezy) <benjamin.taylor@transportforthenorth.com>
2023-08-10 13:41:48 +0100

    Move build to `pyproject.toml`

--- src/caf/toolkit/__init__.py
+++ src/caf/toolkit/__init__.py
@@ -1,7 +1,11 @@
-from . import _version
+"""A toolkit of transport planning and appraisal functionalities."""
 
-__version__ = _version.get_versions()["version"]
+from . import _version
+__version__ = _version.get_versions()['version']
+del _version
+
+# __version__ = "0.0.56"
 
 # Alias
-from caf.toolkit.config_base import BaseConfig
-from caf.toolkit.log_helpers import LogHelper, TemporaryLogFile, ToolDetails, SystemInformation
+# from caf.toolkit.config_base import BaseConfig
+# from caf.toolkit.log_helpers import LogHelper, TemporaryLogFile, ToolDetails, SystemInformation
