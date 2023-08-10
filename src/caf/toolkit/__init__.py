8f4ff66c890c9e71ff6ea91c162bae509e946bb2
BenTaylor-TfN (Sneezy) <benjamin.taylor@transportforthenorth.com>
2023-08-10 18:14:54 +0100

    Get versioningit working with editable installs

--- src/caf/toolkit/__init__.py
+++ src/caf/toolkit/__init__.py
@@ -1,8 +1,6 @@
 """A toolkit of transport planning and appraisal functionalities."""
-
-from importlib.metadata import version
-__version__ = version("caf.toolkit")
+from ._version import __version__
 
 # Alias
-# from caf.toolkit.config_base import BaseConfig
-# from caf.toolkit.log_helpers import LogHelper, TemporaryLogFile, ToolDetails, SystemInformation
+from caf.toolkit.config_base import BaseConfig
+from caf.toolkit.log_helpers import LogHelper, TemporaryLogFile, ToolDetails, SystemInformation
