3e45d12e8b81a185e7303e51964a94f991d22e58
Kieran-Fishwick-TfN <kieran.fishwick@transportforthenorth.com>
2025-03-11 15:33:23 +0000

    adjusted docttests

--- src/caf/toolkit/tqdm_utils.py
+++ src/caf/toolkit/tqdm_utils.py
@@ -40,16 +40,16 @@ def std_out_err_redirect_tqdm():
     >>> bar_format = "{l_bar}{bar}| {n_fmt}/{total_fmt}"
     >>>
     >>> # Redirect stdout to tqdm.write() (don't forget the `as save_stdout`)
-    >>> with std_out_err_redirect_tqdm() as orig_stdout:
+    >>> with std_out_err_redirect_tqdm() as orig_stdout: # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
     ...     # tqdm needs the original stdout
     ...     # and dynamic_ncols=True to autodetect console width
     ...     for i in tqdm(range(3), file=orig_stdout, bar_format=bar_format):
     ...         sleep(.5)
-      0%|          | 0/3
-     33%|###3      | 1/3
-     67%|######6   | 2/3
-    100%|##########| 3/3
-    100%|##########| 3/3
+      0%|...| 0/3
+     33%|#...| 1/3
+     67%|#...| 2/3
+    100%|#...| 3/3
+    100%|#...#| 3/3
     """
     # Init
     orig_out_err = sys.stdout, sys.stderr
