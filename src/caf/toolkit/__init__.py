42fb9201d6e2d5afc85ec2b7db866e8c9dba1d36
Matt Buckley <matt.buckley@transportforthenorth.com>
2026-01-13 10:04:57 +0000

    [lint] ruff format and ruff check --fix

--- src/caf/toolkit/__init__.py
+++ src/caf/toolkit/__init__.py
@@ -6,20 +6,20 @@ __homepage__ = "https://github.com/Transport-for-the-North/caf.toolkit"
 __source_url__ = "https://github.com/Transport-for-the-North/caf.toolkit"
 
 from caf.toolkit import (
-                         arguments,
-                         concurrency,
-                         config_base,
-                         core,
-                         cost_utils,
-                         io,
-                         log_helpers,
-                         math_utils,
-                         pandas_utils,
-                         timing,
-                         toolbox,
-                         tqdm_utils,
-                         translation,
-                         validators,
+    arguments,
+    concurrency,
+    config_base,
+    core,
+    cost_utils,
+    io,
+    log_helpers,
+    math_utils,
+    pandas_utils,
+    timing,
+    toolbox,
+    tqdm_utils,
+    translation,
+    validators,
 )
 from caf.toolkit.config_base import BaseConfig
 from caf.toolkit.log_helpers import LogHelper, SystemInformation, TemporaryLogFile, ToolDetails
